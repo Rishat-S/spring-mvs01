1d3c5284a41b03d7a3acbe28e0eecd78d54e34a9
Rishat Suleymanov <spk_irbis@mail.ru>
2021-07-12 20:10:50 +0300

    Model added

--- src/main/java/ru/lsz/controllers/FirstController.java
+++ src/main/java/ru/lsz/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.lsz.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("massage", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
